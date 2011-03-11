bdd8df680661715c2d24b456d194bcc4a9fcf7d1
Mark Woodall <llad@crab.local>
2011-03-10 21:21:50 -0600

    changing to express

---
+++ #README.md
@@ -0,0 +1,25 @@
+Templay Notes
+=============
+---
+
+Links
+=====
+	* Production[templay.no.de]
+
+Git
+===
+
+for new directory: git init
+git remote add joyent node@templay.no.de:repo
+
+	1. git add server.js
+	2. git commit -m "fix basic errors from jshint"
+	3. git push joyent master
+	
+no.de
+=====
+
+ssh
+---
+ssh node@templay.no.de
+
--- server.js
+++ server.js
@@ -1,34 +1,8 @@
-var http = require('http'),
-    sys = require('sys'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs');
-  
-var server = http.createServer(function (req, res) {
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-    path.exists(filename, function(exists) {
-        if(!exists) {
-            res.writeHead(404, {"Content-Type": "text/plain"});
-            res.write("404 Not Found\n");
-            res.end();
-            return;
-        }
-    });
-    
-    fs.readFile(filename, "binary", function(err, file) {
-        if(err) {
-            res.writeHead(500, {"Content-Type": "text/plain"});  
-            res.write(err + "\n");
-            res.end();  
-            return;  
-        }  
+var app = require('express').createServer();
 
-        res.writeHead(200);  
-        res.write(file, "binary");  
-        res.end();  
-    });
-        
+app.get('/',function(req, res){
+    res.send('hello world');
 });
-  
-server.listen(process.env.PORT || 8001);
+
+app.listen(8000);
+console.log('Express app started on port 8000');
