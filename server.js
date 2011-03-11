c54e9a21382ef12fb16f38688f19e1d677f2d571
Mark Woodall <lladoow@gmail.com>
2011-03-10 21:41:03 -0600

    change to 8001 port

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ app.get('/',function(req, res){
     res.send('hello world');
 });
 
-app.listen(8000);
-console.log('Express app started on port 8000');
+app.listen(8001);
+console.log('Express app started on port 8001');
